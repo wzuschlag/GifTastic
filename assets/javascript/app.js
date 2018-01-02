717ced7c4cd2776c64ac4b83a6c28c12e04dd255
Walter Zuschlag <walterzuschlag@Walters-MacBook-Pro.local>
2018-01-01 19:30:25 -0600

    start on html and js GifTastic HMWK_6.1

---
+++ assets/javascript/app.js
@@ -0,0 +1,10 @@
+// Wait for page to load
+$(document).ready(function()
+{
+	// Initial array of topics
+	var topics = ["Superman", "Wonderwoman", "Batman", "Spiderman", "Catwoman", "Thor", "Black Widow", "Joker", "Harley Quinn"];
+
+
+
+});
+
